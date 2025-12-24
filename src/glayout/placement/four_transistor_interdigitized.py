d2481114d6c5e597ccdedbfedf271fb69daf9c71
Nimish Kapoor <67710754+Nimok15@users.noreply.github.com>
2025-12-24 17:16:42 +0530

    Update four_transistor_interdigitized.py

--- src/glayout/placement/four_transistor_interdigitized.py
+++ src/glayout/placement/four_transistor_interdigitized.py
@@ -4,7 +4,7 @@
 
 from glayout.pdk.mappedpdk import MappedPDK
 from glayout.pdk.sky130_mapped import sky130_mapped_pdk
-from two_transistor_interdigitized import two_nfet_interdigitized, two_pfet_interdigitized
+from .two_transistor_interdigitized import two_nfet_interdigitized, two_pfet_interdigitized
 from typing import Literal, Optional
 from gdsfactory import Component
 from gdsfactory.component_reference import ComponentReference
