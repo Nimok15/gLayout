63119bb22410e90c09e0212952791008cdd9b80e
Nimish Kapoor <67710754+Nimok15@users.noreply.github.com>
2026-01-16 22:45:39 +0530

    Update __init__.py

--- src/glayout/blocks/elementary/current_mirror/__init__.py
+++ src/glayout/blocks/elementary/current_mirror/__init__.py
@@ -1,4 +1,4 @@
-from .current_mirror import (current_mirror, current_mirror_interdigitized_netlist)
+from .current_mirror import current_mirror, current_mirror_interdigitized_netlist
 
 # Backward compatibility
 current_mirror_netlist = current_mirror_interdigitized_netlist
